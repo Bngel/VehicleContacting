7e8b22e05c2d43b05a19e5c51298bbc690b0d958
bngel <bngel1024@gmail.com>
2021-05-29 22:59:32 +0800

    完善首页Banner

--- app/src/main/java/com/example/vehiclecontacting/Adapter/MyBannerAdapter.kt
+++ app/src/main/java/com/example/vehiclecontacting/Adapter/MyBannerAdapter.kt
@@ -1,10 +1,14 @@
 package com.example.vehiclecontacting.Adapter
 
+import android.content.Intent
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.vehiclecontacting.Activity.DiscussActivity
 import com.example.vehiclecontacting.Data.BannerInfo
 import com.example.vehiclecontacting.R
+import com.example.vehiclecontacting.Repository.ActivityCollector
+import com.example.vehiclecontacting.Web.DiscussController.DiscussRepository
 import com.example.vehiclecontacting.Widget.BannerView
 import com.youth.banner.adapter.BannerAdapter
 
@@ -18,6 +22,13 @@ class MyBannerAdapter(images: List<BannerInfo>): BannerAdapter<BannerInfo, MyBan
         else
             holder.view.setImageResource(R.drawable.gp_defaultimg)
         holder.view.setText(data.text)
+        holder.view.setOnClickListener {
+            DiscussRepository.getFirstDiscuss(30, data.number)
+            val discussIntent = Intent(holder.view.context, DiscussActivity::class.java)
+            discussIntent.putExtra("ownerComment", DiscussRepository.ownerComment)
+            discussIntent.putExtra("firstComments", DiscussRepository.firstCommentList)
+            holder.view.context.startActivity(discussIntent)
+        }
     }
 
     override fun onCreateHolder(parent: ViewGroup, viewType: Int): BannerViewHolder {
